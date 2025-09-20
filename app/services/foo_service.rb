34ae1b2ff90cea78ea9427ede356ef5d2197f413
Graham Marlow <graham@mgmarlow.com>
2025-09-20 12:14:11 -0700

    Remove type error, add where type

--- app/services/foo_service.rb
+++ app/services/foo_service.rb
@@ -1,6 +1,11 @@
 class FooService
   def run
-    book = Book.first
-    book.use_undefined_method
+    book_a = Book.where(title: "Foo Bar")
+    book_b = Book
+      .where(status: :completed)
+      .where("updated_at < ?", Date.current)
+      .first
+
+    book_a || book_b
   end
 end
